d88e06b6307eaf6ff3f6823f0a682ad6852cba71
nagendratest <visitnagendra@gmail.com>
2017-01-24 18:07:36 +0530

    updated with email

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -70,29 +70,29 @@ module.exports = {
 	login:function(req,res){
 
 		// Get password and username from request
-        var username = req.param('username');
+        var email = req.param('email');
         var password = req.param('password');
 
         // No username/password entered
-        if(!(username && password)) {
-            return res.send('No username or password specified!', 500);
+        if(!(email && password)) {
+            return res.send('No email or password specified!', 500);
         }
 
         // Lookup the user in the database
 
         User.findOne({
-            username: username
+            email: email
         }).exec(function (err, user) {
 
             // Account not found
             if (err || !user) {
-                return res.send('Invalid username and password combination!', 500);
+                return res.send('Invalid email and password combination!', 500);
             }
 
             // Compare the passwords
             bcrypt.compare(password, user.password, function(err, valid) {
                 if(err || !valid)
-                    return res.send('Invalid username and password combination!', 500)
+                    return res.send('Invalid email and password combination!', 500)
 
                 // The user has authenticated successfully, set their session
                 req.session.authenticated = true;
